a0edf6b7e24566ea1ce5aee65fddae46d445bed2
정동진 <jeongdongjin@jeongdongjin-ui-MacBookPro.local>
2020-12-22 17:55:56 +0900

    12916

--- Level_1/Q_12916.java
+++ Level_1/Q_12916.java
@@ -6,7 +6,21 @@ import org.junit.Test;
 public class Q_12916 {
     boolean solution(String s) {
         boolean answer = true;
+        char[] ch = s.toCharArray();
+        int p = 0;
+        int y = 0;
 
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == 'p' || ch[i] == 'P') {
+                p++;
+            } else if (ch[i] == 'y' || ch[i] == 'Y') {
+                y++;
+            }
+        }
+
+        if (p != y) {
+            answer = false;
+        }
 
         return answer;
     }
